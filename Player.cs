d7831cd0f9c0ffda272f0fee06fdc72bd36ea6d2
Daniel-vdL <alexanderdanailin@gmail.com>
2023-09-08 12:03:37 +0200

    Created Player

---
+++ Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WildWest
+{
+    internal class Player
+    {
+        public string name;
+        public int health = 15;
+        public int money = 5;
+        public int damage = 1;
+        public int armor = 5;
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,14 +2,26 @@
 {
     internal class Program
     {
+
+        public static Player currentPlayer = new Player();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Start();
         }
 
         static void Start()
         {
-            Console.WriteLine();
+            Console.WriteLine("WildWest Adventure");
+            Console.WriteLine("Name:");
+            currentPlayer.name = Console.ReadLine();
+            Console.WriteLine("Fill in start of story here!");
+            if (currentPlayer.name == "")
+                Console.WriteLine("You don't have a name? Strange...");
+
+            else
+                Console.WriteLine("Your name is " + currentPlayer.name);
+
         }
     }
 }
