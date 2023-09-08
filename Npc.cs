4caaed7d3915fb162a13ee792ddb4ec6385cdc28
Daniel-vdL <alexanderdanailin@gmail.com>
2023-09-08 12:39:32 +0200

    Added Npcs
    Added a way to create Npcs and use them.

--- Npc.cs
+++ Npc.cs
@@ -8,5 +8,25 @@ namespace WildWest
 {
     internal class Npc
     {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public int health { get; set; }
+        public int money { get; set; }
+        public int damage { get; set; }
+
+        public string DisplayString { get { return this.ToString(); } }
+
+        public Npc(string Name, int Health, int Money, int Damage)
+        {
+            this.name = Name;
+            this.health = Health;
+            this.money = Money;
+            this.damage = Damage;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {name}, Health: {health}, Money: {money}, Damage: {damage}.";
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,6 +5,13 @@
 
         public static Player currentPlayer = new Player();
 
+        static List<Npc> npcList = new List<Npc>
+        {
+            new Npc("Bob", 15, 100, 5),
+            new Npc("George", 15, 150, 5),
+        };
+
+
         static void Main(string[] args)
         {
             Start();
@@ -16,11 +23,14 @@
             Console.WriteLine("Name:");
             currentPlayer.name = Console.ReadLine();
             Console.WriteLine("Fill in start of story here!");
+            
             if (currentPlayer.name == "")
                 Console.WriteLine("You don't have a name? Strange...");
 
             else
                 Console.WriteLine("Your name is " + currentPlayer.name);
+
+            Console.WriteLine(npcList[1]);
 
         }
     }
