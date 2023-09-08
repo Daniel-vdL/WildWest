e4f240fd832880fab44f503bd2a3a1c440d24122
Daniel-vdL <alexanderdanailin@gmail.com>
2023-09-08 11:32:32 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace WildWest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static void Start()
+        {
+            Console.WriteLine();
+        }
+    }
+}
