61ba08eea4f3969c51af46680f126282bb752494
Daniel-vdL <alexanderdanailin@gmail.com>
2023-09-13 21:49:19 +0200

    Changed Npc and added Enemies

---
+++ Enemies.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WildWest
+{
+    internal class Enemies
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public int health { get; set; }
+        public int money { get; set; }
+        public int damage { get; set; }
+
+        public string DisplayString { get { return this.ToString(); } }
+
+        public Enemies(string Name, int Health, int Money, int Damage)
+        {
+            this.name = Name;
+            this.health = Health;
+            this.money = Money;
+            this.damage = Damage;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {name}, Health: {health}, Money: {money}, Damage: {damage}.";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,20 +5,27 @@
 
         public static Player currentPlayer = new Player();
 
-        static List<Npc> npcList = new List<Npc>
+        static List<Enemies> npcList = new List<Enemies>
         {
-            new Npc("Bob", 15, 100, 5),
-            new Npc("George", 15, 150, 5),
+            new Enemies("Bob", 15, 100, 5),
+            new Enemies("George", 15, 150, 5),
         };
 
 
         static void Main(string[] args)
         {
             Start();
+
         }
 
         static void Start()
         {
+            Npc npc1 = new Npc();
+            npc1.name = "John";
+            npc1.health = 50;
+            npc1.money = 150;
+            npc1.damage = 5;
+
             Console.WriteLine("WildWest Adventure");
             Console.WriteLine("Name:");
             currentPlayer.name = Console.ReadLine();
@@ -28,9 +35,7 @@
                 Console.WriteLine("You don't have a name? Strange...");
 
             else
-                Console.WriteLine("Your name is " + currentPlayer.name);
-
-            Console.WriteLine(npcList[1]);
+                Console.WriteLine($"Your name is {currentPlayer.name}");
 
         }
     }
